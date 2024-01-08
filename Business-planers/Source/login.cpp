2be0827279fa73cdf1ac8c4490d16206d94f15df
nzstoyanov21 <nzstoyanov21@codingburgas.bg>
2024-01-08 22:06:11 +0200

    Fix errors and QoL changes

--- Business-planers/Source/login.cpp
+++ Business-planers/Source/login.cpp
@@ -1,45 +1,62 @@
 #include "../Header Files/login.h"
 
-void login(string username, string password, string passwordVer, bool haveaccount, char loginOrSignup)
+void login(string username, string password, string passwordVer, bool haveAccount, char loginOrSignup)
 {
     string temp = " ";
-    cout << "1. LOGIN" << endl << "2. SIGNUP" << endl << "Press one if you want to log in to an exsisting account or press two if you want to create an account" << endl;
+    cout << "1. LOGIN" << endl << "2. SIGNUP" << endl << "Press one if you want to log in to an existing account or press two if you want to create an account" << endl;
     cin >> loginOrSignup;
     if (loginOrSignup == '1')
     {
-        
+
         cout << "Enter your username: ";
         cin >> username;
+        cout << endl;
         cout << "Enter your password: ";
         cin >> password;
-        cout << endl << "Welcome back " << username << "! What do you want to do?";
+        system("cls");
+        cout << "Welcome back!" << username << " What do you want to do?";
     }
     else if (loginOrSignup == '2')
     {
         cout << "Enter your username: ";
         cin >> username;
+        cout << endl;
         cout << "Enter your password: ";
         cin >> password;
+        cout << endl;
         cout << "Confirm your password: ";
         cin >> passwordVer;
+        cout << endl;
         if (password == passwordVer)
-            cout << "Account created succesfully! Welcome!" << endl;
+        {
+            system("cls");
+            cout << "Account created successfully! Welcome!" << endl;
+        }
         else
         {
+            system("cls");
             cout << "Passwords don't match! Try again!" << endl;
+            sleep(5);
+            system("cls");
             string passwordTries[5];
             for (int i = 0; i < 5; i++)
             {
+                cout << "Confirm your password: ";
                 cin >> passwordTries[i];
+                system("cls");
                 if (passwordTries[i] == password)
                 {
-                    cout << "Account created succesfully! Welcome!" << endl;
+                    cout << "Account created successfully! Welcome!" << endl;
+                    sleep(5);
+                    system("cls");
                     break;
                 }
                 else
                 {
                     cout << "Passwords don't match! Try again!" << endl;
-                    cout << "You have " << 5 - i - 1 << " more tries" << endl;
+                    cout << "You have " << 5 - i - 1 << " more tries";
+                    sleep(5);
+                    system("cls");
                 }
             }
         }
