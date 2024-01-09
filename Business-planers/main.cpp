2ec07aa4bdcd05829bd068308d0efefcbd9d57ca
IVBadalov21 <IVBadalov21@codingburgas.bg>
2024-01-09 20:32:01 +0200

    Fix main cpp

--- Business-planers/main.cpp
+++ Business-planers/main.cpp
@@ -1,23 +1,25 @@
 #include "Header Files/main.h"
 
 
-int main() 
+int main()
 {
-    //TRAN* head = nullptr;
+	TRAN* head = nullptr;
 
-    //// Insert elements into the linked list
-    //insertNode(head, 10);
-    //insertNode(head, 20);
-    //insertNode(head, 30);
+	// Insert elements into the linked list
+	insertNode(head, 10);
+	insertNode(head, 20);
+	insertNode(head, 30);
 
-    //// Display the linked list
-    //std::cout << "Linked List: ";
-    //displayList(head);
+	login(username, password, passwordVer, haveAccount, loginOrSignup);
+	makeTransaction(head, answer, typeOfTran, amount, makeATransaction);
+	
+	// Display the linked list
+	std::cout << "Linked List: ";
+	displayList(head);
 
-    //// Delete the linked list
-    ////deleteList(head);
-    login(username, password, passwordVer, haveAccount, loginOrSignup);
-    makeTransaction( head, answer, typeOfTran, amount, makeATransaction);
+	// Delete the linked list
+	deleteList(head);
 
-    return 0;
+
+	return 0;
 }
